b5a8b5c0e5e3e254c14458212f2c1dcb2a75ac89
Patrick Brochu <patrickbrochu70@gmail.com>
2025-06-05 15:04:51 -0400

    chore: Set up the basic classes(Tree,Node) and the function buildtree
    feat: buildtree now sort and remove dupes from the array
    
    TODO: Continue buildtree function

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-let setup = {
-  print: function () {
-    return "Test";
-  },
-};
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.leftChild = null;
+    this.rightChild = null;
+  }
+}
 
-export { setup };
+class Tree {
+  constructor(array) {
+    this.array = array;
+    this.root = null;
+  }
+}
+
+function buildTree(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    //remove dupes in array
+    for (let x = array.length - 1; x > i; x--) {
+      if (array[i] === array[x]) {
+        array.splice(array[x], 1);
+      }
+    }
+  }
+  array.sort((a, b) => {
+    //sort array in order
+    return a - b;
+  });
+
+  return array;
+}
+
+//Test------------------
+
+console.log(
+  buildTree([1, 1, 1, 1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]),
+);
