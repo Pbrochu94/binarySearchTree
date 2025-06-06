200d32344c65cf723becf4c5e5e256896ab24c8c
Patrick Brochu <patrickbrochu70@gmail.com>
2025-06-06 11:47:07 -0400

    chore: finalized the buildTree function to accurately create a binary tree
    feat:added a console.log function to visualize tree

--- script.js
+++ script.js
@@ -10,30 +10,75 @@ class Tree {
   constructor(array) {
     this.root = this.buildTree(array);
   }
-  buildTree() {
-    for (let i = 0; i < array.length - 1; i++) {
-      //remove dupes in array
-      for (let x = array.length - 1; x > i; x--) {
-        if (array[i] === array[x]) {
-          array.splice(array[x], 1);
+  buildTree(array) {
+    removeDupe(array);
+    //sort array in order
+    array.sort((a, b) => {
+      return a - b;
+    });
+    let root = Math.ceil(array.length / 2); //find root
+    let nodeArray = changeToNode(array);
+    nodeArray.forEach((element) => {
+      compare(element, nodeArray[root]);
+    });
+    return nodeArray[root];
+    //remove dupes in array
+    function removeDupe(array) {
+      for (let i = 0; i < array.length - 1; i++) {
+        for (let x = array.length - 1; x > i; x--) {
+          if (array[i] === array[x]) {
+            array.splice(array[x], 1);
+          }
         }
       }
     }
-    array.sort((a, b) => {
-      //sort array in order
-      return a - b;
-    });
-    let root = Math.ceil(array.length / 2);
-    console.log(array[root]);
-    //create a new Tree var(root)
-    //loop through the array and turn value into nodes
-
-    return array;
+    function changeToNode(array) {
+      let nodeArr = [];
+      //change every value into a node
+      array.forEach((element) => {
+        let valueTurnedNode = new Node(element);
+        nodeArr.push(valueTurnedNode);
+      });
+      array = [];
+      return nodeArr;
+    }
+    function compare(node, comparison) {
+      if (node.value < comparison.value) {
+        if (comparison.leftChild) {
+          return compare(node, comparison.leftChild);
+        }
+        comparison.leftChild = node;
+      }
+      if (node.value > comparison.value) {
+        if (comparison.rightChild) {
+          return compare(node, comparison.rightChild);
+        }
+        comparison.rightChild = node;
+      } else {
+        return;
+      }
+    }
   }
 }
 
 //Test------------------
 
-console.log(
-  buildTree([1, 1, 1, 1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]),
-);
+let tree = new Tree([1, 1, 1, 1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+console.log(tree);
+
+//To visualize tree-----------------
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.rightChild !== null) {
+    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.leftChild !== null) {
+    prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(tree.root);
